f0ff292e6e73a2d60a0b38d58bb71064b261e8ac
rachanajaykumar <rachanajay1999@gmail.com>
2022-05-05 21:22:34 +0530

    first commit

---
+++ projbackend/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import postRoutes from "./routes/posts.js";
+import userRouter from "./routes/user.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.get("/", (req, res) => {
+	res.send("hello to memories api");
+});
+
+app.use("/posts", postRoutes);
+app.use("/user", userRouter);
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+	.connect(process.env.CONNECTION_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() =>
+		app.listen(PORT, () =>
+			console.log(`Server Running on Port: http://localhost:${PORT}`)
+		)
+	)
+	.catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set("useFindAndModify", false);
